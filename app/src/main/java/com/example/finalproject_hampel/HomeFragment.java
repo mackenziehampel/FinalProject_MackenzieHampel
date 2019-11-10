8364ced3d59b6e936b401a3e0f0e4f136301c1f4
Mackenzie <mackenziehampel@weber.edu>
2019-11-10 13:39:19 -0700

    added images

--- app/src/main/java/com/example/finalproject_hampel/HomeFragment.java
+++ app/src/main/java/com/example/finalproject_hampel/HomeFragment.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private View root;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -22,10 +23,12 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        root = inflater.inflate(R.layout.fragment_home, container, false);
+        getActivity().setTitle("Home");
+
+        return  root;
     }
 
 }
